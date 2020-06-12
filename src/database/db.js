c93e309e4a34e5025b494e74cbf077fb607ec366
LeandraOliveiraS <you@example.com>
2020-06-11 21:33:40 -0300

    Explicações e práticas iniciais aula 05

--- src/database/db.js
+++ src/database/db.js
@@ -6,6 +6,7 @@ const sqlite3 = require("sqlite3").verbose()
 
 // criar o objeto que irá fazer operações no banco de dados
 // o caminho passado como parâmetro indica qual o local aonde o banco de dados deve ser criado.
+
 const db = new sqlite3.Database("./src/database/database.db")
 
 module.exports = db
